7e062ecacf4a91bcaa2e362460b1beeda7f28b24
javitr17 <jtruram122@iesmartinezm.es>
2024-02-13 14:56:28 +0100

    entraga1

--- main/models.py
+++ main/models.py
@@ -1,8 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
-class Post():
-    pass
+class Post(models.Model):
 
-class Valoracion():
+    titulo=models.CharField(max_length=100)
+    cuerpo=models.TextField(help_text="Incluye el contenido de tu articulo")
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.titulo} - {self.user}"
+
+    class Meta:
+
+        verbose_name_plural = "Posts"
+
+
+class Valoracion(models.Model):
     RATE_CHOICES = [
         (1,1),
         (2, 2),
@@ -10,3 +23,15 @@ class Valoracion():
         (4, 4),
         (5, 5),
     ]
+    rating=models.PositiveIntegerField(default=1, choices=RATE_CHOICES)
+    comment=models.TextField()
+    fecha_registro=models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+         return f"{self.pk} - {self.post.titulo} - {self.user}"
+
+    class Meta:
+
+         verbose_name_plural = "Valoraciones"
--- main/serializers.py
+++ main/serializers.py
@@ -1,5 +1,14 @@
-class PostSerializer():
-    pass
+from rest_framework import serializers
 
-class ValoracionSerializer():
-   pass
+from .models import Post, Valoracion
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    user=serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Post
+        fields = ['url', 'id', 'titulo', 'cuerpo',
+                  'user']
+
+class ValoracionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Valoracion
+        fields = '__all__'
